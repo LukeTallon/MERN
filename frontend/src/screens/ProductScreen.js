1ec087e6bfce24bd5a669e54313646b73606a542
LukeTallon <luke.tallon@gmail.com>
2022-06-25 20:58:46 +0100

    push4

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,16 +1,25 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 //import { Link } from 'react-router-bootstrap'
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
 import Rating from '../components/Rating'
 import { useParams, Link } from 'react-router-dom'
-import products from '../products'
+import axios from 'axios'
+
 
 const ProductScreen = () => {
   
   const params = useParams();
-  const Product = products.find(p => p._id === params.id);
+  const [Product, setProduct] = useState({})
 
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const {data} = await axios.get(`/api/products/${params.id}`)
   
+      setProduct(data)
+    }
+  
+    fetchProduct()
+  }, [params.id])
 
   return (
   <>
